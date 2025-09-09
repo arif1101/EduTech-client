b94a97edbb2fc13a65a6d5dbe2f5ba06be76a8ad
arif1101 <arifurrahmanarif223@gmail.com>
2025-09-09 12:02:22 +0600

    admin dashboard overview

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -4,7 +4,6 @@ import {
   SidebarContent,
   SidebarGroup,
   SidebarGroupContent,
-  SidebarGroupLabel,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
@@ -12,8 +11,10 @@ import {
   SidebarRail,
 } from "@/components/ui/sidebar"
 import { Link } from "react-router"
-import { useUserInfoQuery } from "@/redux/features/auth/auth.api"
+import { authApi, useLogoutMutation, useUserInfoQuery } from "@/redux/features/auth/auth.api"
 import { getSidebarItems } from "@/utils/getSidebarItems";
+import { Button } from "./ui/button";
+import { useDispatch } from "react-redux";
 
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
@@ -24,23 +25,45 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
     navMain: getSidebarItems(userData?.data?.user?.role)
   }
   // console.log("from app-sidebar :",userData?.data?.user?.role)
-  
+  const dispatch = useDispatch()
+const [logout] = useLogoutMutation()
+  const handleLogout = async () => {
+    await logout(undefined)
+    dispatch(authApi.util.resetApiState())
+  }
   return (
     <Sidebar {...props}>
-      <SidebarHeader>
-        <h1>arif</h1>
+      <SidebarHeader className="">
+        <h1 className="text-4xl font-bold text-sky-500 mx-auto">EduTech</h1>
       </SidebarHeader>
       <SidebarContent>
+        <div className="flex flex-col items-center justify-center border-t pt-2">
+          <div className="grid grid-cols-3 gap-1 my-4 md:my-6">
+            {[...Array(9)].map((_, i) => (
+              <div key={i} className="w-4 h-4 rounded-full bg-gray-400" />
+            ))}
+          </div>
+          <div className="flex flex-col gap-1 text-center">
+            <p className="text-[16px] font-bold mb-2">Airfur</p>
+            <p className="text-[13px] text-gray-500 font-bold">Admin</p>
+            <p className="text-[13px] text-gray-500 font-bold">arif@gmail.com</p>
+            <Button
+            onClick={() => {
+              handleLogout()
+            }}
+             className="bg-red-500 mt-2 hover:bg-red-600">logout</Button>
+          </div>
+        </div>
         {/* We create a SidebarGroup for each parent. */}
         {data.navMain.map((item) => (
           <SidebarGroup key={item.title}>
-            <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
+            {/* <SidebarGroupLabel>{item.title}</SidebarGroupLabel> */}
             <SidebarGroupContent>
               <SidebarMenu>
                 {item.items.map((item) => (
                   <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton asChild >
-                      <Link to={item.url}>{item.title}</Link>
+                    <SidebarMenuButton asChild className="">
+                      <Link className="border w-full mx-auto" to={item.url}>{item.title}</Link>
                     </SidebarMenuButton>
                   </SidebarMenuItem>
                 ))}
--- src/pages/Admin/AdminDashboard.tsx
+++ src/pages/Admin/AdminDashboard.tsx
@@ -1,8 +1,11 @@
+import DashboardGraph from "./DashboardGraph";
+import DashboardStats from "./DashboardStats";
 
 export default function AdminDashboard() {
   return (
-    <div>
-      <h1>admin dashboard</h1>
+    <div className="bg-slate-100 p-6 flex flex-col gap-6">
+      <DashboardStats/>
+      <DashboardGraph/>
     </div>
   )
 }
